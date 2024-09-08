b3a6cebec0670e48b57e2cdeef0f011675f9864f
mynameisKaiser <kaisertsoi@hotmail.com>
2024-09-08 15:34:57 +0800

    submittion 1

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,42 @@
-/**
-*****************************************************************************************************
-* By writing your name in the space provided below, you are honouring the CIS academic honesty pledge 
-* “I have neither given nor received unauthorized aid on this piece of work.”
-* NAME: 
-*****************************************************************************************************
-*/
-
 public class Main {
 
-    public static void main(String[] args) {
-
-    }
-
     // 1. add
-    
+    public static int add(int firstNumber, int secondNumber) {
+        return (firstNumber + secondNumber);
+    }
     // 2. add
-
+    public static int add(int firstNumber, int secondNumber, int thirdNumber, int fourthNumber) {
+        return (add(firstNumber, add(secondNumber, add(thirdNumber, fourthNumber))));
+    }
     // 3. morningGreeting
-
+    public static String morningGreeting(String userName) {
+        System.out.println("早上好"+userName+"!");
+        return userName;
+    }
     // 4. afternoonGreeting
-
+    public static String afternoonGreeting(String userName) {
+        System.out.println("下午好"+userName+"!");
+        return userName;
+    }
     // 5. triple
-
+    public static String triple(String tripleWord) {
+        String triple = (tripleWord+tripleWord+tripleWord);
+        System.out.println(triple);
+        return triple;
+    }
     // 6. half
-
+    public static double half(double a) {
+        System.out.println(a/2);
+        return a/2;
+    }
     // 7. roundPositiveValueToNearestInteger
-
+    public static double roundPositiveValueToNearestInteger(double a) {
+        System.out.println((int)(a+0.5));
+        return a+0.5;
+    }
     // 8. roundNegativeValueToNearestInteger
-
+    public static double roundNegativeValueToNearestInteger(double a) {
+        System.out.println((int)(a-0.5));
+        return a-0.5;
+    }
 }
